e1832866c96b6487d6e2a03781868df0b6a2ecee
硫酸 <lsch001@163.com>
2018-11-12 13:08:27 +0800

    :bug: / window.devicePixelRatio

--- src/util.js
+++ src/util.js
@@ -1,78 +1,84 @@
-import Config from './config';
+'use strict';
 
-export function parseBounds(bounds) {
-  bounds.x = bounds.x ? bounds.x : 0;
-  bounds.y = bounds.y ? bounds.y : 0;
-  if (bounds.width <= 0) {
-    console.warn('PhaserListView: bounds.width <= 0');
-  } else if (bounds.height <= 0) {
-    console.warn('PhaserListView: bounds.height <= 0');
-  }
-  return bounds;
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.parseBounds = parseBounds;
+exports.getWidthOrHeight = getWidthOrHeight;
+exports.capitalizeFirstLetter = capitalizeFirstLetter;
+exports.findChild = findChild;
+exports.detectDrag = detectDrag;
+exports.dispatchClicks = dispatchClicks;
+
+var _config = require('./config');
+
+var _config2 = _interopRequireDefault(_config);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function parseBounds(bounds) {
+    bounds.x = bounds.x ? bounds.x : 0;
+    bounds.y = bounds.y ? bounds.y : 0;
+    if (bounds.width <= 0) {
+        console.warn('PhaserListView: bounds.width <= 0');
+    } else if (bounds.height <= 0) {
+        console.warn('PhaserListView: bounds.height <= 0');
+    }
+    return bounds;
 }
 
 // prefer nominalWidth and nominalHeight
-export function getWidthOrHeight(displayObject, widthOrHeight) {
-  return (
-    displayObject[`nominal${capitalizeFirstLetter(widthOrHeight)}`] ||
-    displayObject[widthOrHeight]
-  );
+function getWidthOrHeight(displayObject, widthOrHeight) {
+    return displayObject['nominal' + capitalizeFirstLetter(widthOrHeight)] || displayObject[widthOrHeight];
 }
 
-export function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export function findChild(children, predicate, scope = null) {
-  if (!children) return false;
-  for (let i = 0; i < children.length; i++) {
-    const child = children[i];
-    if (!child) continue;
-    if (predicate.call(scope, child)) {
-      return child;
+function findChild(children, predicate) {
+    var scope = arguments.length <= 2 || arguments[2] === undefined ? null : arguments[2];
+
+    if (!children) return false;
+    for (var i = 0; i < children.length; i++) {
+        var child = children[i];
+        if (!child) continue;
+        if (predicate.call(scope, child)) {
+            return child;
+        }
+        var found = findChild(child.children, predicate, scope);
+        if (found) {
+            return found;
+        }
     }
-    const found = findChild(child.children, predicate, scope);
-    if (found) {
-      return found;
+    return false;
+}
+
+function detectDrag(pointer) {
+    var distanceX = Math.abs(pointer.positionDown.x - pointer.positionUp.x);
+    var distanceY = Math.abs(pointer.positionDown.y - pointer.positionUp.y);
+    var time = pointer.timeUp - pointer.timeDown;
+    return distanceX > _config2.default.AUTO_DETECT_THRESHOLD || distanceY > _config2.default.AUTO_DETECT_THRESHOLD;
+}
+
+function dispatchClicks(pointer, clickables, type) {
+    if (type == 'onInputUp' && detectDrag(pointer)) return;
+    // SEARCH OBJECT UNDER POINT AS THERE IS NO CLICK PROPAGATION SUPPORT IN PHASER
+    var found = findChild(clickables, function(clickable) {
+        var pt = clickable.worldPosition;
+        var anchor = clickable.anchor;
+        var pivot = clickable.pivot;
+
+        var width = clickable.width / window.devicePixelRatio;
+        var height = clickable.height / window.devicePixelRatio;
+        var scale = clickable.scale;
+        var x = pt.x - (anchor ? anchor.x * width : 0) - pivot.x * scale.x;
+        var y = pt.y - (anchor ? anchor.y * height : 0) - pivot.y * scale.y;
+        // console.log('does ', x, y, clickable.width, clickable.height, ' intersect ', pointer.x, pointer.y)
+        return clickable.inputEnabled && new Phaser.Rectangle(x, y, width, height).contains(pointer.x, pointer.y);
+    });
+    if (found && found.events && found.events[type] && found.events[type].dispatch) {
+        found.events[type].dispatch(found, pointer, true);
     }
-  }
-  return false;
-}
-
-export function detectDrag(pointer) {
-  const distanceX = Math.abs(pointer.positionDown.x - pointer.positionUp.x);
-  const distanceY = Math.abs(pointer.positionDown.y - pointer.positionUp.y);
-  const time = pointer.timeUp - pointer.timeDown;
-  return (
-    distanceX > Config.AUTO_DETECT_THRESHOLD ||
-    distanceY > Config.AUTO_DETECT_THRESHOLD
-  );
-}
-
-export function dispatchClicks(pointer, clickables, type) {
-  if (type == 'onInputUp' && detectDrag(pointer)) return;
-  // SEARCH OBJECT UNDER POINT AS THERE IS NO CLICK PROPAGATION SUPPORT IN PHASER
-  const found = findChild(clickables, clickable => {
-    const pt = clickable.worldPosition;
-    const { anchor, pivot, width, height, scale } = clickable;
-    const x = pt.x - (anchor ? anchor.x * width : 0) - pivot.x * scale.x;
-    const y = pt.y - (anchor ? anchor.y * height : 0) - pivot.y * scale.y;
-    // console.log('does ', x, y, clickable.width, clickable.height, ' intersect ', pointer.x, pointer.y)
-    return (
-      clickable.inputEnabled &&
-      new Phaser.Rectangle(x, y, clickable.width, clickable.height).contains(
-        pointer.x,
-        pointer.y
-      )
-    );
-  });
-  if (
-    found &&
-    found.events &&
-    found.events[type] &&
-    found.events[type].dispatch
-  ) {
-    found.events[type].dispatch(found, pointer, true);
-  }
-  return found;
+    return found;
 }
